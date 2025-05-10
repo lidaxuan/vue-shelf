59c1464b53bd221aa241348f4e21a3e1e010410b
lijixuan <lijixuan@easyliao-inc.com>
2025-05-10 15:00:11 +0800

    feat: config

--- vue.config.js
+++ vue.config.js
@@ -22,15 +22,11 @@ function getSassVar() {
 }
 
 const port = 6537; // dev port
-
 let publicPath = '/';
 
 if (process.env.NODE_ENV !== 'development') {
   // publicPath = process.env.VUE_APP_OS_domain ? `//${process.env.VUE_APP_OS_domain}` : '/';
 }
-
-// const sdkChooseLoader = require( './src/customLoader/testReplace.js' );
-// const errorLoader = require( './src/customLoader/errorLoader.js' );
 module.exports = {
   publicPath,
   runtimeCompiler: true,
@@ -72,11 +68,7 @@ module.exports = {
     hot: true,
     disableHostCheck: true,
   },
-  configureWebpack: {
-    // resolveLoader: {
-    //   modules: ['node_modules', './src/customLoader/'],
-    // },
-  },
+  configureWebpack: {},
   chainWebpack(config) {
     config.plugins.delete('preload'); // TODO: need test
     config.plugins.delete('prefetch'); // TODO: need test
@@ -86,53 +78,26 @@ module.exports = {
     config.module.rule('pug').use('pug-plain-loader').loader('pug-plain-loader').end();
 
     config.module.rule('svg').exclude.add(resolve('src/icons')).end();
-    // config.module.rule('error').test(/\.vue$/).exclude.add(resolve('src/customLoader/errorLoader')).end();
-    // config.module.rule('myLoader').test(/\.js$/).use('testReplace').loader('testReplace').end();
-    // config.module.rule('errorLoader').test(/\.js$/).use('errorLoader').loader('errorLoader').end();
-    config.module
-      .rule('icons')
-      .test(/\.svg$/)
-      .include.add(resolve('src/icons'))
-      .end()
-      .use('svg-sprite-loader')
-      .loader('svg-sprite-loader')
-      .options({
-        symbolId: 'icon-[name]'
-      })
-      .end();
+    config.module.rule('icons').test(/\.svg$/).include.add(resolve('src/icons')).end()
+      .use('svg-sprite-loader').loader('svg-sprite-loader').options({symbolId: 'icon-[name]'}).end();
     // set preserveWhitespace
-    config.module
-      .rule('vue')
-      .use('vue-loader')
-      .loader('vue-loader')
-      .tap(options => {
-        options.compilerOptions.preserveWhitespace = true;
-        return options;
-      })
-      .end();
+    config.module.rule('vue').use('vue-loader').loader('vue-loader').tap(options => {
+      options.compilerOptions.preserveWhitespace = true;
+      return options;
+    }).end();
 
-    config
-      // https://webpack.js.org/configuration/devtool/#development
-      .when(process.env.NODE_ENV === 'development', conf => conf.devtool('cheap-source-map'));
+    config.when(process.env.NODE_ENV === 'development', conf => conf.devtool('cheap-source-map'));
     config.resolve.alias
       .set('@', resolve('src'))
       .set('assets', resolve('src/assets'))
       .set('@lijixuan/gojs', resolve('src/components/go'))
       .set('utils', resolve('src/utils'));
-
-    // config.plugin('provide').use(webpack.ProvidePlugin, [{
-    //   'window.Quill': 'quill'
-    // }]);
   },
   css: {
-    // 是否使用css分离插件 ExtractTextPlugin
     extract: process.env.NODE_ENV === 'development' ? false : true,
-    // 开启 CSS source maps?
     sourceMap: process.env.NODE_ENV === 'development' ? true : false,
-    // css预设器配置项
     loaderOptions: {
       sass: {
-        // @是src的别名
         prependData: getSassVar()
       }
     },
